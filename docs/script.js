c200c1bff55f7a2313a1c559d787cbc2d50d4710
Edgar Nuñez <eanunezt@gmail.com>
2019-09-29 18:47:06 -0500

    Aplicación de colores

---
+++ docs/script.js
@@ -0,0 +1,82 @@
+var colorRandomSize = 5;
+
+function randomPalette(){
+    var gradeInterval =  1 / colorRandomSize;
+    var r = getRandom(0, 255);
+    var g = getRandom(0, 255);
+    var  b = getRandom(0, 255);
+    var hsl = rgbToHsl(r, g, b);
+    var colorsHex = [];
+
+    for (var i = 0; i < colorRandomSize; i++) {
+        hsl[0] = getNextHue(hsl[0], gradeInterval);
+        var rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+        var hex = rgbToHex(rgb[0], rgb[1], rgb[2]).toUpperCase();
+        colorsHex.push(hex);
+    }
+
+    generateRules(colorsHex);
+}
+
+function generateRules(colors){
+   
+    var rules = [
+        `.website-background{ color: ${colors[0]};}`,
+        `.element-text{ color: ${colors[1]};}`,
+        `.element-border{ border-color: ${colors[2]};}`,
+        `.element-background{ background-color: ${colors[3]};}`,
+        `.header{ color: ${colors[4]};}`
+    ];
+    for (var i = 0; i < colors.length; i++) {
+        $(`#color${i + 1}`).css('background-color', colors[i]);
+    }
+    $("textarea#css-rules").val(rules.join('\r\n\n'));
+
+}
+
+
+function limpiarPaleta() {
+    var colors = ['#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF'];
+    generateRules(colors);
+}
+
+// Numeros aleatorios entre un minimo y un maximo, incluyendo los dos numeros
+function getRandom(minimo, maximo) {
+    return Math.floor(Math.random() * (maximo - (minimo + 1)) + minimo);
+}
+
+function getNextHue(h, distance) {
+    h += distance;
+    h = h > 1 ? h - 1: h;
+    return h;
+}
+
+/*var conjugate = (1.0 + Math.sqrt(5.0)) / 2.0;
+function getNextHue(int m)
+{
+  hue = (hue + conjugate*m) % 1.0f;
+  return hue;
+}*/
+
+
+
+function convertToHex(c) {
+    c = parseInt(c);
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+/**
+ * Converts an RGB color value to Hexadecimal.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns hex to be an 8 digit hexadecimal number
+ * 
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @returns String  hex     The hex representation    
+ */
+function rgbToHex(r, g, b) {
+    var hex = "#" + convertToHex(r) + convertToHex(g) + convertToHex(b);
+    return hex;
+}
